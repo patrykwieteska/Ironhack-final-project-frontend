3e707403b45e260bfaede0356cf99f6eccd048ea
patrykwieteska <patrykwieteskapw@gmail.com>
2021-12-16 07:56:22 +0100

    added prediction possibility

--- src/app/components/predictions/prediction-item/prediction-item.component.ts
+++ src/app/components/predictions/prediction-item/prediction-item.component.ts
@@ -1,7 +1,7 @@
 import { PredictionService } from 'src/app/services/predictions/prediction.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PredictedMatch } from './../../../model/app-models/match/PredictedMatch';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-prediction-item',
@@ -10,9 +10,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PredictionItemComponent implements OnInit {
 
+  isPredictionModeOn:boolean=false;
+
   predictionForm: FormGroup;
   homeGoals: FormControl;
   awayGoals: FormControl;
+  predictionHomeGoals!:number;
+
+  homeGoalsDefault: any;
+  awayGoalsDefault: any;
 
   constructor(private predictionService: PredictionService) {
 
@@ -23,16 +29,74 @@ export class PredictionItemComponent implements OnInit {
       homeGoals:this.homeGoals,
       awayGoals:this.awayGoals
     })
+
+   
+
+
+
+    console.log(this.prediction)
+
    }
 
   @Input()
+  @Output()
   prediction!:PredictedMatch
 
   ngOnInit(): void {
+
+    if(this.prediction.predictionInfo.prediction===null || this.prediction.predictionInfo.prediction===undefined) {
+      console.log('NULLOWE' + this.prediction.fixture.fixtureId)
+      this.homeGoalsDefault ='';
+      this.awayGoalsDefault ='';
+    }  else {
+      console.log('OK'+ this.prediction.fixture.fixtureId)
+      this.homeGoalsDefault = this.prediction.predictionInfo.prediction.predictedHomeGoals;
+      this.awayGoalsDefault = this.prediction.predictionInfo.prediction.predictedAwayGoals;
+    }
+
+    
+    this.homeGoals = new FormControl(this.homeGoalsDefault,[Validators.required,Validators.pattern("^[0-9]*$"),Validators.maxLength(2)]);
+    this.awayGoals = new FormControl(this.awayGoalsDefault,[Validators.required,Validators.pattern("^[0-9]*$"),Validators.maxLength(2)]);
+
+    this.predictionForm = new FormGroup({
+      homeGoals:this.homeGoals,
+      awayGoals:this.awayGoals
+    })
+    
   }
+
+
 
   predictMatch():void {
 
+
+    let fixture:number = this.prediction.fixture.fixtureId; 
+    let homeGoals:number = this.homeGoals.value; 
+    let awayGoals:number = this.awayGoals.value;; 
+    
+    this.predictionService.predictMatch(homeGoals,awayGoals,fixture).subscribe(
+      result => {
+        console.log('RESULT', result)
+        this.showAlert();
+      }, err => {
+        console.log('ERROR', err)
+      }
+    )
+
+
   }
+
+  
+  public isVisible: boolean = false;
+
+  showAlert() : void {
+    if (this.isVisible) { // if the alert is visible return
+      return;
+    } 
+    this.isVisible = true;
+    setTimeout(()=> this.isVisible = false,2500); // hide the alert after 2.5s
+  }
+
+
 
 }
--- src/app/model/app-models/match/PredictedMatch.ts
+++ src/app/model/app-models/match/PredictedMatch.ts
@@ -3,6 +3,12 @@ import { UserPrediction } from '../predictions/UserPrediction';
 
 import { Match } from 'src/app/model/app-models/match/Match';
 export class PredictedMatch {
+        public get predictionInfo(): PredictionInfo {
+            return this._predictionInfo;
+        }
+        public set predictionInfo(value: PredictionInfo) {
+            this._predictionInfo = value;
+        }
 
         public get fixture(): Match {
             return this._fixture;
